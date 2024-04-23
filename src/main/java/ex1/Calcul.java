6f255f412fef0cc8efd47d2538e7e803a2b77f47
Guioll <guillaume.ollivier2022@campus-eni.fr>
2024-04-23 15:59:34 +0200

    Exercice 1

---
+++ src/main/java/ex1/Calcul.java
@@ -0,0 +1,26 @@
+package ex1;
+
+public class Calcul {
+
+    /**
+     * Calcul la somme de deux entiers
+     *
+     * @param a Le premier entier à additionner
+     * @param b Le deuxième entier à additionner
+     * @return La somme des deux paramètres
+     */
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Calcul la différence entre deux entiers
+     *
+     * @param a Le premier entier
+     * @param b Le deuxième entier à soustraire
+     * @return La différence entre les deux paramètres
+     */
+    public static int soustraction(int a, int b) {
+        return a - b;
+    }
+}
---
+++ src/main/java/ex1/Entreprise.java
@@ -0,0 +1,21 @@
+package ex1;
+
+import java.util.Date;
+
+public class Entreprise {
+
+    public static final int CAPITAL_MAX = 3000000;
+    public int siret;
+    public String nom;
+    public String adresse;
+    public Date date_creation;
+
+
+    /**
+     * Affiche le statut de l'entreprise
+     */
+    public void afficherStatut() {
+
+    }
+
+}
